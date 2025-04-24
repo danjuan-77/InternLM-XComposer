fc98ec14bc8aefcab3cb4445eaca897ffc4533de
danjuan-77 <youngertutu@gmail.com>
2025-04-24 19:48:05 +0800

    update

--- InternLM-XComposer-2.5-OmniLive/inference_audio.py
+++ InternLM-XComposer-2.5-OmniLive/inference_audio.py
@@ -1,5 +1,5 @@
 import os
-os.environ['TRANSFORMERS_TRUST_REMOTE_CODE'] = '1'
+
 os.environ["USE_HF"] = "True"
 
 import torch
@@ -13,9 +13,7 @@ from swift.llm import (
 from swift.utils import seed_everything
 
 model_type = ModelType.qwen2_audio_7b_instruct
-model_id_or_path = (
-    "/share/nlp/tuwenming/projects/InternLM-XComposer/internlm-xcomposer2d5-ol-7b"
-)
+model_id_or_path = "internlm/internlm-xcomposer2d5-ol-7b"
 template_type = get_default_template_type(model_type)
 print(f"template_type: {template_type}")
 
@@ -24,7 +22,6 @@ model, tokenizer = get_model_tokenizer(
     torch.float16,
     model_id_or_path=model_id_or_path,
     model_dir="audio",
-    trust_remote_code=True,
     model_kwargs={"device_map": "cuda:0"},
 )
 model.generation_config.max_new_tokens = 256
