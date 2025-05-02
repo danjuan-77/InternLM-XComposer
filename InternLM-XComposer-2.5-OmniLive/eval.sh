836529d68ce0fd3baa85d4d6c4bad450ec626281
danjuan-77 <youngertutu@gmail.com>
2025-05-02 15:29:44 +0800

    11

--- InternLM-XComposer-2.5-OmniLive/eval.sh
+++ InternLM-XComposer-2.5-OmniLive/eval.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=3
 
-python examples/eval_audio.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LAQA
+# python examples/eval_audio.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_1/LAQA
+
+python examples/eval_audio.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_2/MAIC
