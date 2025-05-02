f4a1068f60bd017f2e0d40ae7a3f541be280e8f9
danjuan-77 <youngertutu@gmail.com>
2025-05-02 15:40:56 +0800

    update

--- InternLM-XComposer-2.5-OmniLive/examples/eval_image.py
+++ InternLM-XComposer-2.5-OmniLive/examples/eval_image.py
@@ -10,7 +10,202 @@ from example_code.utils import auto_configure_device_map
 
 torch.set_grad_enabled(False)
 
+import tempfile
+import traceback
+import json
+import os
+from tqdm import tqdm
+from typing import List, Optional
+from moviepy.editor import (
+    VideoFileClip,
+    AudioFileClip,
+    concatenate_audioclips,
+    ImageClip,
+    concatenate_videoclips,
+)
+import argparse
 
+
+tempfile.tempdir = "/share/nlp/tuwenming/projects/HAVIB/tmp"
+maic_cls_list = ['bus', 'hair-dryer', 'pipa', 'man', 'ambulance', 'razor', 'harp', 'tabla', 'bass', 'handpan', 
+        'girl', 'sitar', 'car', 'lion', 'guitar', 'vacuum-cleaner', 'cat', 'mower', 'helicopter', 'boy', 'drum', 
+        'keyboard', 'tuba', 'saw', 'flute', 'cello', 'woman', 'gun', 'accordion', 'violin', 'clarinet', 'erhu', 
+        'saxophone', 'guzheng', 'dog', 'baby', 'horse', 'male', 'wolf', 'bird', 'ukulele', 'piano', 'female', 
+        'marimba', 'not sure', 'no available option']
+
+mvic_cls_list = ['sushi', 'banana', 'cake', 'butterfly', 'bird', 'microphone', 'hamburger', 'pineapple', 
+        'man', 'book', 'sunglasses', 'goat', 'tie', 'cabinetry', 'motorcycle', 'drawer', 'strawberry', 
+        'sheep', 'pasta', 'parrot', 'bull', 'table', 'penguin', 'watch', 'pillow', 'shellfish', 'kangaroo', 
+        'flower', 'paddle', 'rocket', 'helicopter', 'bus', 'mushroom', 'bee', 'tree', 'boat', 'saxophone', 
+        'football', 'lizard', 'violin', 'dog', 'cucumber', 'cello', 'airplane', 'horse', 'drum', 'box', 
+        'rabbit', 'car', 'door', 'orange', 'shelf', 'camera', 'poster', 'lemon', 'cat', 'fish', 'bread', 
+        'piano', 'apple', 'glasses', 'bicycle', 'truck', 'deer', 'woman', 'wheelchair', 'cheese', 'chair', 
+        'plate', 'tomato', 'bed', 'starfish', 'balloon', 'bottle', 'crab', 'beer', 'frog', 'shrimp', 'tower', 
+        'guitar', 'pig', 'peach', 'train', 'pumpkin', 'elephant', 'jellyfish', 'parachute', 'monkey', 'flag',
+        'not sure', 'no available option']
+
+prompt_avl = """
+        In each video frame, there may be multiple categories of sound-emitting instances. Each category can have several instances. 
+        You can choose instance categories from the given categories list.
+        The naming format for instances is: category_id. Instance IDs start from 1, e.g., male_1, dog_2, dog_3, cat_4. 
+        It is crucial that the instance names (i.e., category_id) remain consistent for the same instances across different frames.
+        The bbox format is: [x, y, w, h], where x and y represent the coordinates of the top-left corner, and w and h are the width and height. 
+        The final answer must strictly adhere to the following format: 
+        answer={"frame_0": {"guzheng_1": "[269, 198, 83, 16]", "guzheng_2": "[147, 196, 75, 13]", "female_3": "[152, 108, 123, 36]"}, "frame_1": ..., "frame_n": ...}
+    """
+
+avl_cls_list = ['dog', 'clarinet', 'banjo', 'cat', 'guzheng', 'tree', 'lion', 'tuba', 
+        'ukulele', 'flute', 'piano', 'person', 'violin', 'airplane', 'bass', 'pipa', 
+        'trumpet', 'accordion', 'saxophone', 'car', 'lawn-mower', 'cello', 'bassoon', 
+        'horse', 'guitar', 'erhu', 'not sure', 'no available option']
+
+prompt_avlg = """
+        Please output the answer in a format that exactly matches the following example:
+        answer={'frame_0': [x0, y0, w0, h0], 'frame_1': None, ..., 'frame_9': [x9, y9, w9, h9]}.
+        Note, for [x, y, w, h], where x and y represent the top-left corner of the bounding box, 
+        and w and h represent the width and height of the bounding box.
+    """
+
+avqa_cls_list = ['ukulele', 'cello', 'clarinet', 'violin', 'bassoon', 'accordion', 'banjo', 'tuba', 'flute', 'electric_bass', 'bagpipe', 
+        'drum', 'congas', 'suona', 'xylophone', 'saxophone', 'guzheng', 'trumpet', 'erhu', 'piano', 'acoustic_guitar', 'pipa', 'not sure', 'no available option']
+
+havib_constants = {
+    'L1_LAQA': {
+        'options_sound_clarity': ['first', 'last', 'same', 'not sure'],
+        'options_sound_order': ['sound', 'noise', 'not sure'],
+        'options_sound_volume': ['first', 'last', 'same', 'not sure'],
+        'options_yes_no': ['yes', 'no', 'not sure'],
+    },
+
+    'L1_LIQA': {
+        'get_from_background_binary': ['yes', 'no', 'not sure'],
+        'get_from_image_binary': ['yes', 'no', 'not sure'],
+        'get_from_foreground_binary': ['yes', 'no', 'not sure'],
+        'get_from_image_triple': ['blurred', 'normal', 'clear', 'not sure'],
+        'get_from_3d-task1': ['center', 'left', 'right', 'not sure'],
+        'get_from_3d-task2': ['cone', 'cube', 'cylinder', 'cuboid', 'no available option', 'not sure'],
+        # 'get_from_3d-task3': [0, 1, 2, 3, 4, 5, 6],
+        'get_from_space_hard': ['center', 'top left', 'top center', 'top right', 'bottom left', 'bottom center', 'bottom right', 'no available option', 'not sure'],
+        'get_from_color': ['blue', 'green', 'red', 'puprle', 'yellow', 'no available option', 'not sure'],
+        'get_yes_no': ['yes', 'no', 'not sure'],
+        # 'get_lines_count': [0, 1, 2, 3, 4],
+        'get_lines_direction': ['horizontal', 'vertical', 'inclined', 'not sure'],
+        'get_from_space_easy_area': ['the right one', 'the left one', 'the middle one', 'the bottom one', 'the top one'],
+        'get_from_space_easy_bbrightness': ['the right one', 'the left one', 'the middle one', 'the bottom one', 'the top one'],
+        'options_yes_no': ['yes', 'no', 'not sure'],
+    },
+
+    'L1_LVQA': {
+        'which_object': ['square', 'circle', 'triangle', 'not sure', 'no available option', 'not sure'],
+        'what_shape': ['Triangular pyramid', 'Cone', 'Cube', 'Sphere', 'None', 'not sure'],
+        # 'how_many': [0, 1, 2, 3, 4, 5, 6, 7, 8],
+        'what_movement_2d': ['horizontal', 'inclined', 'vertical', 'no movenment', 'None', 'not sure'],
+        'what_movement_3d': ['Rotation', 'Shrinking', 'Translation', 'Enlarging', 'None', 'not sure'],
+        'what_surface': ['Rough', 'Moderate', 'Smooth', 'None', 'not sure'],
+        'spacial_change': ['Bottom-left to top-right', 'Bottom-right to top-left', 'Top-left to bottom-right', 'Top-right to bottom-left', 'None', 'not sure', 'No movement',],
+        'options_yes_no': ['yes', 'no', 'not sure'],
+    },
+
+    'L2_MAIC': {
+        'maic_cls_list': maic_cls_list,
+        'prompt_maic': "There may be one or more sound-emitting objects in the provided audio. \nPlease strictly output the answer in the format answer={'cls_1': count_1, 'cls_2': count_2}, \nfor example, answer={'dog': 2, 'cat': 3, 'male': 1}. \n"
+    },
+
+    'L2_MVIC': {
+        'mvic_cls_list': mvic_cls_list,
+        'prompt_mvic': "There may be one or more visible objects in the provided image. \nPlease strictly output the answer in the format answer={'cls_1': count_1, 'cls_2': count_2}, \nfor example, answer={'dog': 2, 'cat': 3, 'male': 1}. \n Possible categoris are in the list: mvic_cls_list"
+    },
+
+    'L3_AVH': {
+        'prompt_avh': "Please answer the question based on the given audio and video.",
+        'avh_options_list': ['yes', 'no', 'not sure'],
+    },
+
+    'L3_VAH': {
+        'prompt_vah': "Please answer the question based on the given audio and video.",
+        'vah_options_list': ['yes', 'no', 'not sure'],
+    },
+
+    'L3_AVL': {
+        'prompt_avl': prompt_avl,
+        'avl_cls_list': avl_cls_list,
+    },
+
+    'L3_AVM': {
+        'prompt_avm': 'Please answer the question based on the given audio and video.',
+        'avm_options_list': ['yes', 'no', 'not sure'],
+    },
+
+    'L3_AVR': {
+        'prompt_avr': "Please output the indices of the images list, starting from 0. For example: [], or [0, 3], or [1, 4, 9]."
+    },
+
+    'L3_VAR': {
+        'prompt_var': "Please output the indices of the wavs list, starting from 0. For example: [], or [0, 3], or [1, 4, 9]."
+    },
+
+    'L4_AVC': {
+
+    },
+
+    'L4_AVLG': {
+        'prompt_avlg': prompt_avlg,
+    },
+
+    'L4_AVQA': {
+        'avqa_options_list_is': ['yes', 'no', 'not sure'],
+    },
+
+    'L5_AVLG': {
+        'prompt_avlg': prompt_avlg,
+    },
+
+    'L5_AVQA': {
+        'avqa_cls_list': avqa_cls_list,
+        'avqa_options_list_is': ['yes', 'no', 'not sure'],
+    },
+}
+
+def get_real_path(task_path: str, src_path: str) -> str:
+    """传入taskpath和一些文件的path，构造文件的真实path
+
+    Args:
+        task_path (str): task path
+        src_path (str): 每个文件的path
+
+    Returns:
+        str: 文件的真实path
+    """
+    temp_path = os.path.join(task_path, src_path)
+    return os.path.normpath(temp_path)
+
+def get_real_options_or_classes(d: dict) -> str:
+    """Replace pseudo-options with real options text."""
+    opts = d['input']['question'].get('options')
+    if opts in havib_constants.get(d['task'], {}):
+        opts = havib_constants[d['task']][opts]
+    if opts:
+        label = 'semantic categories' if 'cls' in opts else 'options'
+        return f"Available {label} are: {opts}"
+    return ''
+
+def get_real_prompt(d: dict) -> str:
+    """Replace pseudo-prompt with real prompt text."""
+    prm = d['input']['question'].get('prompt')
+    if prm in havib_constants.get(d['task'], {}):
+        prm = havib_constants[d['task']][prm]
+    return prm or ''
+
+def get_real_input(d: dict) -> str:
+    """Concatenate prompt, options, and question text into one input string."""
+    prompt = get_real_prompt(d)
+    options = get_real_options_or_classes(d)
+    question = d['input']['question']['text'] or ''
+    # 去掉多余的句点
+    parts = [p for p in (prompt, options, question) if p]
+    return " ".join(parts)
+
+######################## Above are help function tools
 
 def init_model():
     model_path = 'internlm-xcomposer2d5-ol-7b/base'
@@ -24,30 +219,87 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--num_gpus", default=1, type=int)
     parser.add_argument("--dtype", default='fp16', type=str)
+    parser.add_argument("--task_path", type=str, required=True, help="Path to the task folder containing data.json and media files")
+    
     args = parser.parse_args()
 
     model = init_model()
-
-
     if args.dtype == 'fp16':
         model.half().cuda()
     elif args.dtype == 'fp32':
         model.cuda()
-
     if args.num_gpus > 1:
         from accelerate import dispatch_model
         device_map = auto_configure_device_map(args.num_gpus)
-        model, tokenizer = dispatch_model(model, device_map=device_map)
-
-    
+        model, tokenizer = dispatch_model(model, device_map=device_map)    
     model.tokenizer = tokenizer
 
-    question = 'Analyze the given image in a detail manner'
-    image = ['../examples/dubai.png']
+    task_path = args.task_path
+    task_name = f"L{task_path.rsplit('/', 1)[0][-1]}_{task_path.rsplit('/', 1)[-1]}"
+    model_name = "IXC2.5-OL"
+    save_prediction_json = f'/share/nlp/tuwenming/projects/HAVIB/eval/user_outputs/{model_name}/tasks/{task_name}.json'
+    os.makedirs(os.path.dirname(save_prediction_json), exist_ok=True)
+    print('>>> save res to:', save_prediction_json)
 
-    with torch.autocast(device_type='cuda', dtype=torch.float16):
-        response, _ = model.chat(tokenizer, question, image, do_sample=False, num_beams=3, use_meta=True)
-    print(response)
+    data_json_path = os.path.join(task_path, "data.json")
+    with open(data_json_path, "r", encoding='utf-8') as f:
+        raw_data = json.load(f)
+    print(">>>Finished load raw data...")
+    parsed_data = []
+    for item in raw_data:
+        inp = item.get('input', {})
+        entry = {
+            'id': item.get('id'),
+            'task': item.get('task'),
+            'subtask': item.get('subtask', None),
+            'text': get_real_input(item),
+            'audio_list': inp.get('audio_list', None),
+            'image_list': inp.get('image_list', None),
+            'video': inp.get('video', None)
+        }
+        parsed_data.append(entry)
+
+    print(">>>Finished parse raw data...")    
+    
+    predictions = []
+    
+    for data in tqdm(parsed_data):
+        _id = data['id']
+        _task = data['task']
+        _subtask = data['subtask']
+        text = data['text']
+        audio_list = (
+            [get_real_path(task_path, p) for p in data["audio_list"]]
+            if data["audio_list"] else None
+        )
+        image_list = (
+            [get_real_path(task_path, p) for p in data["image_list"]]
+            if data["image_list"] else None
+        )
+        video = (
+            get_real_path(task_path, data['video'])
+            if data['video'] else None
+        )
+        print(f">>> text input=:{text}")
+
+
+
+        question = 'Analyze the given image in a detail manner'
+        image = ['../examples/dubai.png']
+
+        with torch.autocast(device_type='cuda', dtype=torch.float16):
+            response, _ = model.chat(tokenizer, question, image, do_sample=False, num_beams=3, use_meta=True)
+        
+        pred_record = {
+            "task": _task,
+            "subtask": _subtask,
+            "id": _id,
+            "predict": response,
+        }
+        print('>>> ans=:', pred_record)
+        
+    with open(save_prediction_json, 'w', encoding='utf-8') as json_file:
+        json.dump(predictions, json_file, ensure_ascii=False, indent=4)
 
 # Expected output
 """
