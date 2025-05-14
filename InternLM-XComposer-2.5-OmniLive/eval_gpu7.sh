7845e4d164982ec251732e5f6cf11b93ef69de96
danjuan-77 <youngertutu@gmail.com>
2025-05-14 16:31:01 +0800

    update

--- InternLM-XComposer-2.5-OmniLive/eval_gpu7.sh
+++ InternLM-XComposer-2.5-OmniLive/eval_gpu7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=3
 
-python examples/eval_video.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_5/AVQA
+python examples/eval_video.py --task_path /share/nlp/tuwenming/projects/HAVIB/data/levels/level_6/AVSQA_v
 
 # nohup bash eval_gpu7.sh > /share/nlp/tuwenming/projects/HAVIB/logs/eval_IXC2.5-OL_gpu7_$(date +%Y%m%d%H%M%S).log 2>&1 &
